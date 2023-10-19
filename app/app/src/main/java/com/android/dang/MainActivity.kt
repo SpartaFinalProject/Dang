c056497a6d75569b8360660c8980b381b8f288d7
kwonyongil <kkwonyi95@gmail.com>
2023-10-19 16:22:35 +0900

    [Feat] : 카카오 맵 api 연결

--- app/app/src/main/java/com/android/dang/MainActivity.kt
+++ app/app/src/main/java/com/android/dang/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.android.dang
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.dang.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_view, ShelterFragment())
+            .commit()
     }
 }
