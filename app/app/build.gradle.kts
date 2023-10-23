9265e0766602e61b145b5d54c1b72166aaba5a8b
이예원 <81704418+scarletYewon@users.noreply.github.com>
2023-10-23 14:25:06 +0900

    [FEATURE] navigation

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.android.dang"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.android.dang"
@@ -33,6 +33,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -44,4 +48,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
 }
--- app/app/src/main/java/com/android/dang/MainActivity.kt
+++ app/app/src/main/java/com/android/dang/MainActivity.kt
@@ -2,10 +2,36 @@ package com.android.dang
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.android.dang.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportFragmentManager.beginTransaction().replace(binding.fragmentView.id, BlankFragment()).commit()
+
+        binding.navBar.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.menu_home -> {
+                    supportFragmentManager.beginTransaction().replace(binding.fragmentView.id, BlankFragment()).commit()
+                }
+                R.id.menu_shelter -> {
+                    supportFragmentManager.beginTransaction().replace(binding.fragmentView.id, BlankFragment2()).commit()
+                }
+                R.id.menu_like -> {
+                    supportFragmentManager.beginTransaction().replace(binding.fragmentView.id, BlankFragment3()).commit()
+                }
+                R.id.menu_dictionary -> {
+                    supportFragmentManager.beginTransaction().replace(binding.fragmentView.id, BlankFragment4()).commit()
+                }
+            }
+            true
+        }
+        binding.
     }
 }
