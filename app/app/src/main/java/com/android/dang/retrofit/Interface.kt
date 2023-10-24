261087a6ec86bf0d101ffeaf2859f5a786a3a426
kwonyongil <kkwonyi95@gmail.com>
2023-10-24 20:26:08 +0900

    [FEAT]: retrofit 설정

--- app/app/src/main/java/com/android/dang/retrofit/Interface.kt
+++ app/app/src/main/java/com/android/dang/retrofit/Interface.kt
@@ -1,7 +1,9 @@
-package com.android.dangtheland.retrofit
+package com.android.dang.retrofit
 
-import com.android.dangtheland.retrofit.abandonedDog.AbandonedDog
-import com.android.dangtheland.retrofit.kind.Kind
+import com.android.dang.retrofit.abandonedDog.AbandonedDog
+import com.android.dang.retrofit.abandonedDog.AbandonedDogRes
+import com.android.dang.retrofit.kind.Kind
+import com.android.dang.retrofit.sido.SidoRes
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -10,17 +12,43 @@ interface Interface {
 
     @GET("abandonmentPublic")
     fun abandonedDogSearch(
-        @Query("serviceKey") serviceKey : String = Constants.AUTH_HEADER,
-        @Query("upkind") upkind : Int = 417000,
-        @Query("_type") type : String = "json",
-        @Query("numOfRows") numOfRows : Int,
-        @Query("kind") kind : String
-    ): Call<AbandonedDog?>
+        @Query("serviceKey") serviceKey: String = Constants.AUTH_HEADER,
+        @Query("upkind") upkind: Int = 417000,
+        @Query("_type") type: String = "json",
+        @Query("numOfRows") numOfRows: Int,
+        @Query("upr_cd") uprCode: String? = null,
+        @Query("org_cd") orgCode: String? = null,
+    ): Call<AbandonedDogRes?>
+
+//    @GET("abandonmentShelter")
+//        fun abandonedDogShelter(
+//        @Query("serviceKey") serviceKey: String = Constants.AUTH_HEADER,
+//        @Query("upkind") upkind: Int = 417000,
+//        @Query("_type") type: String = "json",
+//        @Query("numOfRows") numOfRows: Int,
+//        @Query("upr_cd") uprCode: String? = null,
+//        @Query("org_cd") orgCode: String? = null,
+//        ) : Call<AbandonedDog?>
 
     @GET("kind")
     fun kindSearch(
-        @Query("serviceKey") serviceKey : String = Constants.AUTH_HEADER,
-        @Query("up_kind_cd") kind : Int = 417000,
-        @Query("_type") type : String = "json"
+        @Query("serviceKey") serviceKey: String = Constants.AUTH_HEADER,
+        @Query("up_kind_cd") kind: Int = 417000,
+        @Query("_type") type: String = "json"
     ): Call<Kind?>
+
+    @GET("sido")
+    fun getSidoList(
+        @Query("serviceKey") serviceKey: String = Constants.AUTH_HEADER,
+        @Query("numOfRows") numOfRows: Int? = null,
+        @Query("pageNum") pageNum: Int? = null,
+        @Query("_type") type: String = "json"
+    ): Call<SidoRes>
+
+    @GET("sigungu")
+    fun getSigunguList(
+        @Query("serviceKey") serviceKey: String = Constants.AUTH_HEADER,
+        @Query("upr_cd") code: String,
+        @Query("_type") type: String = "json"
+    ): Call<SidoRes>
 }
