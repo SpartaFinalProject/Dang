49b0a12697863368186c3343d9021be57cdd6f0c
이예원 <81704418+scarletYewon@users.noreply.github.com>
2023-11-02 21:36:30 +0900

    [FIX] item cardView 적용
    radius 8dp

--- app/app/src/main/java/com/android/dang/home/homeAdapter/HomeAdapter.kt
+++ app/app/src/main/java/com/android/dang/home/homeAdapter/HomeAdapter.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet.Layout
 import androidx.recyclerview.widget.RecyclerView
